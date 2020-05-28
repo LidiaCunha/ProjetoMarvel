816ba29a817b0a196fb02a4e69f0297aafd419bb
Lidia Gomes Cunha <lidiagomes121212@gmail.com>
2020-05-28 02:06:01 -0300

    Consumo de API da Marvel

--- js/script.js
+++ js/script.js
@@ -1,36 +1,53 @@
-const criarCard = (arrayInfo) => {
+//Função que insere card no html 
+const criarCard = (objetoArray) => {
    const card = `<div class="card">
+        <input type="checkbox">
+        <div class="toggle">+</div>
         <figure class="cardImagem">
-            <img src="${arrayInfo.thumbnail.path}/standard_large.${arrayInfo.thumbnail.extension}" alt=${arrayInfo.name}>
+            <img src="${objetoArray.thumbnail.path}/standard_fantastic.${objetoArray.thumbnail.extension}" alt=${objetoArray.title} title=${objetoArray.title}>
         </figure>
-        <div class="cardTexto">
-            <span>${arrayInfo.name}</span>
+        <div class="details">
+            <h2>DESCRIÇÃO</h2><br>
+            ${objetoArray.description}
         </div>
     </div>`;
 
     return card;
 }
 
+//Função que recebe array como parametro
 const inserirHTML = (dadosApi) => {
     const listaCard = document.getElementById('listaCard');
     listaCard.innerHTML = " ";
-    dadosApi.forEach(elemento => {
-        listaCard.innerHTML += criarCard(elemento);
-    });
+    //Verificando se objeto do array existe
+    if (dadosApi.length == 0){
+        alert("Erro de busca: A pesquisa não está correta ou o quadrinho não existe.")
+    } else{
+        //Se elemento existir passe o objeto para função CriarCard
+        dadosApi.forEach(elemento => {
+            listaCard.innerHTML += criarCard(elemento);
+        });
+    }
 }  
 
+//Função para requisitar a url 
 const getMarvel = async () => {
     const pesquisa = document.getElementById('pesquisa').value;
-    const timestime = "1589910124";
+    //Regras de utilização da API Marvel: chave pública, chave privada e timestamp
+    const timestamp = "1589910124";
     const apikey = "55107d58b8f180551b90d6a24b3b608d";
     const md5 = "a95e0c0753d49c665be4ae74d74bfb92";
-    const url = `http://gateway.marvel.com/v1/public/events?ts=${timestime}&apikey=${apikey}&hash=${md5}&limit=100`;
+    const url = `http://gateway.marvel.com/v1/public/events?nameStartsWith=${pesquisa}&ts=${timestamp}&apikey=${apikey}&hash=${md5}&limit=100`;
 
+    //Utilizando fetch para url e guardando em uma variavel
     const getApi = await fetch(url);
+    //Transformando "getApi" em json e guardando em uma variavel
     const json = await getApi.json();
 
-    console.log (json);
+    //Passando array como argumento para função
     inserirHTML(json.data.results);
 }
 
 document.getElementById('botao').addEventListener("click", getMarvel);
+
+
